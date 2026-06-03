3aedb1cb808129d501545d264adb947ff782c38a
AamerRiaz15 <aamerriaz22@gmail.com>
2026-06-03 19:38:58 +0100

    Implement accumulator program for user inputs

--- intermediate/loops/accumulator_program/main.py
+++ intermediate/loops/accumulator_program/main.py
@@ -1,1 +1,23 @@
+accum = 0
+count = 0
 
+while True:
+    user_input = input('Enter a number or type "done": ')
+
+    if user_input.lower() == 'done':
+        break
+
+    try:
+        converted_input = float(user_input)
+    except:
+        print('Invalid Input. Please enter a number.')
+        continue
+
+    accum += converted_input
+    count += 1
+    
+average = accum / count
+
+print('Total:', accum)
+print('Average:', average)
+print('Number of values entered:', count)
